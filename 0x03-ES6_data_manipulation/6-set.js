79eb6f3be0f1ef1a6de7af16fb1aa605374f7bd0
joewacira <josephwmuthoni@gmail.com>
2024-08-02 22:33:34 +0000

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Joseph Muthoni<https://github.com/joewacira>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
