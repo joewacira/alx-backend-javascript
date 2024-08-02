2d3266a4102610fcdcfd254823f04dc3df24d925
joewacira <josephwmuthoni@gmail.com>
2024-08-02 22:41:56 +0000

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves a map of grocery names and their quantity.
+ * @author Joseph Muthoni <https://github.com/joewacira>
+ * @returns {Map<string, Number>}
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
+
